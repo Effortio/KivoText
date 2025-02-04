89b2d34b488f9f222ef06e026ab41657757ad377
Effortio <103933115+Effortio@users.noreply.github.com>
2025-02-04 23:17:26 +0800

    Revert "import sql"
    This reverts commit 00f1eb84afee1e46621240a16f743f4bd7dc58f3.

--- structure.sql
+++ structure.sql
@@ -34,14 +34,6 @@ CREATE TABLE `accessinfo` (
   `deleteable` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否允许用户删除文件'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- 转存表中的数据 `accessinfo`
---
-
-INSERT INTO `accessinfo` (`id`, `filelengthlimit`, `filescountlimit`, `deleteable`) VALUES
-(2, 20000, 100, 1),
-(3, 20000, 100, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -70,13 +62,6 @@ CREATE TABLE `readsharedfilelog` (
   `comment` tinytext COMMENT '注释'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- 转存表中的数据 `readsharedfilelog`
---
-
-INSERT INTO `readsharedfilelog` (`id`, `userid`, `fileid`, `readtime`, `comment`) VALUES
-(9, 3, 78, '2024-03-21 13:23:56', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -90,32 +75,6 @@ CREATE TABLE `sharedinfo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 转存表中的数据 `sharedinfo`
---
-
-INSERT INTO `sharedinfo` (`fileid`, `sharecode`, `avaliable`) VALUES
-(1, 'WBA2BA', 0),
-(67, 'LZ7HN1', 1),
-(68, 'XBIPOI', 1),
-(69, '9NLOFX', 1),
-(70, 'MKOWFB', 0),
-(71, 'VFF1AG', 1),
-(72, '16BBZV', 0),
-(73, '75ZBJT', 0),
-(74, 'LDYXKG', 0),
-(75, '47LE9D', 1),
-(76, 'EK7JH0', 1),
-(77, 'GV741J', 0),
-(78, 'M4TZCT', 1),
-(82, 'PHP9KS', 0),
-(83, '4N3GRT', 0),
-(84, 'MQ2ESV', 0),
-(88, 'V9PSFP', 0),
-(98, 'BZB33J', 0);
-
--- --------------------------------------------------------
-
---
 -- 表的结构 `users`
 --
 
@@ -127,14 +86,6 @@ CREATE TABLE `users` (
   `lastloginIP` tinytext NOT NULL COMMENT '上次登录IP',
   `token` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- 转存表中的数据 `users`
---
-
-INSERT INTO `users` (`id`, `name`, `password`, `lastonlinetime`, `lastloginIP`, `token`) VALUES
-(2, 'Leoministry', 0x37633461386430396361333736326166363165353935323039343364633236343934663839343162, '2024-08-21 22:44:28', '38.207.136.53', 'gJ3n1qt1IjbwxzC0'),
-(3, 'jamesmay', 0x37633461386430396361333736326166363165353935323039343364633236343934663839343162, '2024-03-21 21:23:45', '183.213.85.222', '');
 
 --
 -- 转储表的索引
